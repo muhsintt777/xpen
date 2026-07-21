5c472d9ca591e87047f8a0adfb41867e62fb30cb
Muhsin <muhsintt777@gmail.com>
2026-07-21 19:27:04 +0530

    chore: export db connection

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -2,14 +2,14 @@ import { Pool } from 'pg';
 import { ENV } from '@/configs/env.js';
 import { ConsoleUtils } from '@/utils/console-utils.js';
 
-const pool = new Pool({
+export const db = new Pool({
   connectionString: ENV.DB_URL,
   ssl: ENV.DB_URL ? { rejectUnauthorized: false } : false,
 });
 
 export async function connectDb(): Promise<void> {
   ConsoleUtils.logInfo('Connecting to the database...');
-  const client = await pool.connect();
+  const client = await db.connect();
   try {
     await client.query('SELECT NOW()');
   } finally {
@@ -20,6 +20,6 @@ export async function connectDb(): Promise<void> {
 
 export async function disconnectDb(): Promise<void> {
   ConsoleUtils.logInfo('Disconnecting from the database...');
-  await pool.end();
+  await db.end();
   ConsoleUtils.logSuccess('Database connection closed successfully');
 }
