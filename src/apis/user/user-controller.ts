e9bcad6c4a6e43fbea3354b625ca23be67a51fc9
Muhsin <muhsintt777@gmail.com>
2026-08-12 23:05:55 +0530

    fix: update parameter name for getAllUsers method

--- src/apis/user/user-controller.ts
+++ src/apis/user/user-controller.ts
@@ -3,7 +3,7 @@ import { UserService } from './user-service.js';
 import { ApiResponse } from '@/utils/api-response.js';
 
 export class UserController {
-  static async getAllUsers(req: Request, res: Response) {
+  static async getAllUsers(_req: Request, res: Response) {
     const result = await UserService.getAllUsers();
     res.status(200).json(new ApiResponse(result));
   }
