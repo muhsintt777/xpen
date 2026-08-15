728b0f540376ca47b78dcddb406d6b0b4aad681c
Muhsin <muhsintt777@gmail.com>
2026-08-15 23:24:10 +0530

    chore: code refactor

--- src/main/app-router.ts
+++ src/main/app-router.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { expenseRouter } from '@/apis/expense/expense-router.js';
 import { userRouter } from '@/apis/user/user-router.js';
-import { authRouter } from '@/apis/auth/auth-routes.js';
+import { authRouter } from '@/apis/auth/auth-router.js';
 import { categoryRouter } from '@/apis/category/category-router.js';
 
 const router: Router = Router();
@@ -11,3 +11,5 @@ router.use('/expense', expenseRouter);
 router.use('/category', categoryRouter);
 
 export { router as appRouter };
+
+// todo: merge api utils
